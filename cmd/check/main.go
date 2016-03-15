a0d39a84783678f091d37008e509ff902065d99f
Leon de Jager <ldejager@cisco.com>
2016-03-15 09:41:25 +0000

    Removed uncommented code

--- cmd/check/main.go
+++ cmd/check/main.go
@@ -177,11 +177,6 @@ func parseRepository(repository string) (string, string) {
 		} else {
 			return officialRegistry, segs[0] + "/" + segs[1]
 		}
-		// if strings.Contains(segs[0], ":") {
-		// 	return segs[0], segs[1]
-		// } else {
-		// 	return officialRegistry, segs[0] + "/" + segs[1]
-		// }
 	case 1:
 		return officialRegistry, "library/" + segs[0]
 	}
